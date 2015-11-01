40425f04c927899c6f04de3b758c4234d3094b75
Matt Boynes <mboynes@alleyinteractive.com>
2015-11-01 01:31:16 -0400

    Don't override default rules

--- inc/rewrites.php
+++ inc/rewrites.php
@@ -7,19 +7,19 @@
  * @return array Rewrite Testing tests, with our custom tests added in.
  */
 function wcnyc_rewrite_tests( $tests ) {
-  return array(
-    'Recipes' => array(
-      '/recipes/' => 'index.php?post_type=recipe',
-      '/recipes/eggplant/' => 'index.php?post_type=recipe&primary-ingredient=$matches[1]',
-      '/recipes/eggplant/eggplant-parmesan/' => 'index.php?primary-ingredient=$matches[1]&recipe=$matches[2]&page=$matches[3]',
-    ),
-    'Dish Types' => array(
-      '/recipes/dessert/' => array(
-        'match' => 'index.php?post_type=recipe&primary-ingredient=$matches[1]',
-        'query' => array( 'post_type' => 'recipe', 'dish-type' => 'dessert' ),
-      ),
-    ),
-  );
+	$tests['Recipes'] = array(
+		'/recipes/' => 'index.php?post_type=recipe',
+		'/recipes/eggplant/' => 'index.php?post_type=recipe&primary-ingredient=$matches[1]',
+		'/recipes/eggplant/eggplant-parmesan/' => 'index.php?primary-ingredient=$matches[1]&recipe=$matches[2]&page=$matches[3]',
+	);
+	$tests['Dish Types'] = array(
+		'/recipes/dessert/' => array(
+			'match' => 'index.php?post_type=recipe&primary-ingredient=$matches[1]',
+			'query' => array( 'post_type' => 'recipe', 'dish-type' => 'dessert' ),
+		),
+	);
+
+	return $tests;
 }
 add_filter( 'rewrite_testing_tests', 'wcnyc_rewrite_tests' );
 
