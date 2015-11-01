cdebdbc991a91605f36f4d6f2cec4ba210787a2b
Matt Boynes <mboynes@alleyinteractive.com>
2015-11-01 01:41:37 -0400

    Add dish types

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -24,5 +24,26 @@ function wcnyc_taxonomies() {
 		),
 		'rewrite' => array( 'slug' => 'recipes' ),
 	) );
+
+	register_taxonomy( 'dish-type', array( 'recipe' ), array(
+		'labels' => array(
+			'name'                  => __( 'Dish Types', 'wcnyc-2015' ),
+			'singular_name'         => __( 'Dish Type', 'wcnyc-2015' ),
+			'search_items'          => __( 'Search Dish Types', 'wcnyc-2015' ),
+			'popular_items'         => __( 'Popular Dish Types', 'wcnyc-2015' ),
+			'all_items'             => __( 'All Dish Types', 'wcnyc-2015' ),
+			'parent_item'           => __( 'Parent Dish Type', 'wcnyc-2015' ),
+			'parent_item_colon'     => __( 'Parent Dish Type', 'wcnyc-2015' ),
+			'edit_item'             => __( 'Edit Dish Type', 'wcnyc-2015' ),
+			'view_item'             => __( 'View Dish Type', 'wcnyc-2015' ),
+			'update_item'           => __( 'Update Dish Type', 'wcnyc-2015' ),
+			'add_new_item'          => __( 'Add New Dish Type', 'wcnyc-2015' ),
+			'new_item_name'         => __( 'New Dish Type Name', 'wcnyc-2015' ),
+			'add_or_remove_items'   => __( 'Add or remove Dish Types', 'wcnyc-2015' ),
+			'choose_from_most_used' => __( 'Choose from most used Dish Types', 'wcnyc-2015' ),
+			'menu_name'             => __( 'Dish Types', 'wcnyc-2015' ),
+		),
+		'rewrite' => array( 'slug' => 'recipes' ),
+	) );
 }
 add_action( 'init', 'wcnyc_taxonomies' );
