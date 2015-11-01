b210fb1250467b361db5a2e2cf7525b378cb5917
Matt Boynes <mboynes@alleyinteractive.com>
2015-11-01 01:41:56 -0400

    Add tests for dish types

--- inc/rewrites.php
+++ inc/rewrites.php
@@ -12,8 +12,13 @@ function wcnyc_rewrite_tests( $tests ) {
 		'/recipes/eggplant/' => 'index.php?post_type=recipe&primary-ingredient=$matches[1]',
 		'/recipes/eggplant/eggplant-parmesan/' => 'index.php?primary-ingredient=$matches[1]&recipe=$matches[2]&page=$matches[3]',
 	);
+	$tests['Dish Types'] = array(
+		'/recipes/dessert/' => array(
+			'match' => 'index.php?post_type=recipe&primary-ingredient=$matches[1]',
+			'query' => array( 'post_type' => 'recipe', 'dish-type' => 'dessert' ),
+		),
+	);
 
 	return $tests;
 }
 add_filter( 'rewrite_testing_tests', 'wcnyc_rewrite_tests' );
-
