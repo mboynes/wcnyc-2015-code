47815cbbbeda460c096b10078d54bb31c322fe64
Matt Boynes <mboynes@alleyinteractive.com>
2015-11-01 01:45:56 -0400

    Remove tests

--- inc/rewrites.php
+++ inc/rewrites.php
@@ -1,19 +1,2 @@
 <?php
 
-/**
- * Add tests for our rewrite rules.
- *
- * @param  array $tests Rewrite Testing tests.
- * @return array Rewrite Testing tests, with our custom tests added in.
- */
-function wcnyc_rewrite_tests( $tests ) {
-	$tests['Recipes'] = array(
-		'/recipes/' => 'index.php?post_type=recipe',
-		'/recipes/eggplant/' => 'index.php?post_type=recipe&primary-ingredient=$matches[1]',
-		'/recipes/eggplant/eggplant-parmesan/' => 'index.php?primary-ingredient=$matches[1]&recipe=$matches[2]&page=$matches[3]',
-	);
-
-	return $tests;
-}
-add_filter( 'rewrite_testing_tests', 'wcnyc_rewrite_tests' );
-
