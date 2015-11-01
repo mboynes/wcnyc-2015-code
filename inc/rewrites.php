e711f46fb41f0a48dc18fba31fbcea39a94cf608
Matt Boynes <mboynes@alleyinteractive.com>
2015-11-01 09:09:16 -0500

    Merge step-4

--- inc/rewrites.php
+++ inc/rewrites.php
@@ -22,3 +22,18 @@ function wcnyc_rewrite_tests( $tests ) {
 	return $tests;
 }
 add_filter( 'rewrite_testing_tests', 'wcnyc_rewrite_tests' );
+
+/**
+ * Improve the primary ingredient rewrite rules to be limited to recipes.
+ *
+ * @param  array $rules The default rewrite rules for primary ingredients.
+ * @return array The filtered rules.
+ */
+function wcnyc_primary_ingredient_rewrite_rules( $rules ) {
+	foreach ( $rules as $regex => $rule ) {
+		$rules[ $regex ] = str_replace( 'index.php?', 'index.php?post_type=recipe&', $rule );
+	}
+
+	return $rules;
+}
+add_filter( 'primary-ingredient_rewrite_rules', 'wcnyc_primary_ingredient_rewrite_rules' );
