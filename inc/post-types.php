0c69e8b9a956bffd4af556efd35ba5a878dc47fe
Matt Boynes <mboynes@alleyinteractive.com>
2015-11-01 01:34:28 -0400

    Step 1

--- inc/post-types.php
+++ inc/post-types.php
@@ -21,31 +21,6 @@ function wcnyc_post_types() {
 		),
 		'public' => true,
 		'taxonomies' => array( 'primary-ingredient' ),
-		'rewrite' => array( 'slug' => 'recipes/%primary-ingredient%' ),
 	) );
-
-	// The recipes 'archive'
-	add_rewrite_rule( "recipes/?$", "index.php?post_type=recipe", 'top' );
-	add_rewrite_rule( "recipes/{$GLOBALS['wp_rewrite']->pagination_base}/([0-9]{1,})/?$", 'index.php?post_type=recipe&paged=$matches[1]', 'top' );
 }
 add_action( 'init', 'wcnyc_post_types' );
-
-/**
- * Add the primary ingredient to the post type links for recipes.
- *
- * @param  string $permalink The post's permalink.
- * @param  object $post WP_Post object for this permalink.
- * @return string Modified permalink.
- */
-function wcnyc_post_type_link( $permalink, $post ) {
-	if ( 'recipe' == $post->post_type ) {
-		$primary_ingredient = get_the_terms( $post, 'primary-ingredient' );
-		if ( ! is_wp_error( $primary_ingredient ) && ! empty( $primary_ingredient ) ) {
-			$primary_ingredient = reset( $primary_ingredient );
-			$permalink = str_replace( '%primary-ingredient%', $primary_ingredient->slug, $permalink );
-		}
-	}
-
-	return $permalink;
-}
-add_filter( 'post_type_link', 'wcnyc_post_type_link', 10, 2 );
--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -4,7 +4,7 @@
  * Setup all the taxonomies for our theme.
  */
 function wcnyc_taxonomies() {
-	register_taxonomy( 'primary-ingredient', array( 'recipe', 'post' ), array(
+	register_taxonomy( 'primary-ingredient', array( 'recipe' ), array(
 		'labels' => array(
 			'name'                  => __( 'Primary Ingredients', 'wcnyc-2015' ),
 			'singular_name'         => __( 'Primary Ingredient', 'wcnyc-2015' ),
@@ -22,28 +22,6 @@ function wcnyc_taxonomies() {
 			'choose_from_most_used' => __( 'Choose from most used Primary Ingredients', 'wcnyc-2015' ),
 			'menu_name'             => __( 'Primary Ingredients', 'wcnyc-2015' ),
 		),
-		'rewrite' => array( 'slug' => 'recipes' ),
-	) );
-
-	register_taxonomy( 'dish-type', array( 'recipe' ), array(
-		'labels' => array(
-			'name'                  => __( 'Dish Types', 'wcnyc-2015' ),
-			'singular_name'         => __( 'Dish Type', 'wcnyc-2015' ),
-			'search_items'          => __( 'Search Dish Types', 'wcnyc-2015' ),
-			'popular_items'         => __( 'Popular Dish Types', 'wcnyc-2015' ),
-			'all_items'             => __( 'All Dish Types', 'wcnyc-2015' ),
-			'parent_item'           => __( 'Parent Dish Type', 'wcnyc-2015' ),
-			'parent_item_colon'     => __( 'Parent Dish Type', 'wcnyc-2015' ),
-			'edit_item'             => __( 'Edit Dish Type', 'wcnyc-2015' ),
-			'view_item'             => __( 'View Dish Type', 'wcnyc-2015' ),
-			'update_item'           => __( 'Update Dish Type', 'wcnyc-2015' ),
-			'add_new_item'          => __( 'Add New Dish Type', 'wcnyc-2015' ),
-			'new_item_name'         => __( 'New Dish Type Name', 'wcnyc-2015' ),
-			'add_or_remove_items'   => __( 'Add or remove Dish Types', 'wcnyc-2015' ),
-			'choose_from_most_used' => __( 'Choose from most used Dish Types', 'wcnyc-2015' ),
-			'menu_name'             => __( 'Dish Types', 'wcnyc-2015' ),
-		),
-		'rewrite' => true,
 	) );
 }
 add_action( 'init', 'wcnyc_taxonomies' );
